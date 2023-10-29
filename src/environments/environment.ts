b28c530ccd39bf66e1683b4264961282540e9a53
Pedro Vergara <pe.vergarag@duocuc.cl>
2023-10-29 20:52:00 -0300

    quitar credenciales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBJCH1uoufqAcb9Crw1-bGL0xpXOin_5b4",
-    authDomain: "appmovil-c7a40.firebaseapp.com",
-    projectId: "appmovil-c7a40",
-    storageBucket: "appmovil-c7a40.appspot.com",
-    messagingSenderId: "443147875477",
-    appId: "1:443147875477:web:4fd3e3c658638a785f579c",
-    measurementId: "G-CT7L5P4NGN"
-  }
-};
+//ingrese sus credenciales aqui ......¡¡¡¡¡¡¡¡¡¡¡
 
 /*
  * For easier debugging in development mode, you can import the following file
